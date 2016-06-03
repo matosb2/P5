023676a59906fbe318896febefc0bf2b5651b174
matosb2 <justmats@gmail.com>
2016-06-02 21:47:12 -0400

    Add map marker, tooltip, info window

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,29 @@
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.753, lng: -74.144 },
-        zoom: 8
+
+
+    var myLatlng = new google.maps.LatLng(40.753, -74.144);
+    var mapOptions = {
+        zoom: 15,
+        center: myLatlng
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var contentString = '<h1 id="firstHeading" class="firstHeading">Kearny</h1>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title: "Adventure Begins"
     });
+    marker.addListener('click', function(){
+    	infowindow.open(map, marker);
+    });
+
+    // To add the marker to the map, call setMap();
+    marker.setMap(map);
 }
