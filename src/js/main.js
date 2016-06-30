0509d2099295eb9ec7d4360ff45ad1e0361ee4d0
matosb2 <justmats@gmail.com>
2016-06-29 22:44:05 -0400

    Move knockout applybindings to end of initMap because initMap function needs to finish firing off

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-var locations = [
-{
+var locations = [{
     name: "Red Bull Arena",
     address: "600 Cape May St, Harrison, NJ 07029",
     lat: 40.737046,
@@ -29,38 +28,37 @@ var locations = [
     address: "4 Pennsylvania Plaza, New York, NY 10001",
     lat: 40.750691,
     long: -73.993476
-}
-];
+}];
 
 
 //VIEW MODEL
 var Location = function(data) {
-	this.name = ko.observable(data.title);
-	this.address = ko.observable(data.address);
-	this.lat = ko.observable(data.lat);
-	this.long = ko.observable(data.long);
-}
+    this.name = ko.observable(data.title);
+    this.address = ko.observable(data.address);
+    this.lat = ko.observable(data.lat);
+    this.long = ko.observable(data.long);
+};
 
 var viewModel = function() {
-	var self = this;
+    var self = this;
 
-	this.locationList = ko.observableArray([]);
-	locations.forEach(function(locationItem) {
-		self.locationList.push(new Location(locationItem));
-	});
+    this.locationList = ko.observableArray([]);
+    locations.forEach(function(locationItem) {
+        self.locationList.push(new Location(locationItem));
+    });
 
-	this.currentLocation = ko.observable(this.locationList()[0]);
-	this.setLocation = function (clickedLocation) {
-		self.currentLocation(clickedLocation);
-		google.maps.event.trigger(clickedLocation.marker, 'click');
-	};
+    this.currentLocation = ko.observable(this.locationList()[0]);
+    this.setLocation = function(clickedLocation) {
+        self.currentLocation(clickedLocation);
+        google.maps.event.trigger(clickedLocation.marker, 'click');
+    };
 
-	this.places = ko.observableArray(locations);
-	this.query = ko.observable("");
+    this.places = ko.observableArray(locations);
+    this.query = ko.observable("");
 
 };
 
-ko.applyBindings(viewModel);
+
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -71,21 +69,37 @@ function initMap() {
     var infowindow = new google.maps.InfoWindow();
 
     var marker;
+    
+    gmarkers = [];
 
-    for (i = 0; i < locations.length; i++) {
+    function createMarker(latlng, html) {
+    	latlng = new google.maps.LatLng(locations[i].lat, locations[i].long);
         marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i].lat, locations[i].long),
+            position: latlng,
             map: map
         });
 
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent(locations[i].name);
-                infowindow.open(map, marker);
-            };
-        })(marker, i));
+        google.maps.event.addListener(marker, 'click', function() {
+        	infowindow.setContent(html);
+        	infowindow.open(map, marker);
+    	});
+    return marker;
     }
+
+
+    for (i = 0; i < locations.length; i++) {
+        gmarkers[locations[i][0]] =
+            createMarker(new google.maps.LatLng(locations[i][2], locations[i][3]), locations[i][0] + "<br>" + locations[i][1]);
+    }
+
+
+
+    /*google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            infowindow.setContent(locations[i].name);
+            infowindow.open(map, marker);
+        };
+    })(marker, i));*/
+
+ko.applyBindings(new viewModel());
 }
-
-
-
