5bee4dba2c807a9736440e20ebdb3d8217337067
matosb2 <justmats@gmail.com>
2016-08-02 23:00:29 -0400

    Remove marker from global scope, add marker animations

--- src/js/main.js
+++ src/js/main.js
@@ -2,32 +2,38 @@ var locations = [{
     name: "Red Bull Arena",
     address: "600 Cape May St, Harrison, NJ 07029",
     lat: 40.737046,
-    long: -74.150361
+    long: -74.150361,
+    marker: ''
 }, {
     name: "MetLife Stadium",
     address: "1 MetLife Stadium Dr, East Rutherford, NJ 07073",
     lat: 40.813091,
-    long: -74.074209
+    long: -74.074209,
+    marker: ''
 }, {
     name: "World Trade Center",
     address: "285 Fulton St, New York, NY 10007",
     lat: 40.713175,
-    long: -74.013104
+    long: -74.013104,
+    marker: ''
 }, {
     name: "Zeppelin Hall Biergarten",
     address: "88 Liberty View Dr, Jersey City, NJ 07302",
     lat: 40.715120,
-    long: -74.046754
+    long: -74.046754,
+    marker: ''
 }, {
     name: "Prudential Center",
     address: "25 Lafayette St, Newark, NJ 07102",
     lat: 40.733617,
-    long: -74.171150
+    long: -74.171150,
+    marker: ''
 }, {
     name: "Madison Square Garden",
     address: "4 Pennsylvania Plaza, New York, NY 10001",
     lat: 40.750691,
-    long: -73.993476
+    long: -73.993476,
+    marker: ''
 }];
 
 
@@ -62,17 +68,17 @@ var viewModel = function() {
     });
 
     self.search = ko.computed(function() {
-    	for(var i = 0; i < locations.length; i++) {
-    		locations[i].marker.setVisible(true);
-    	}
-    	return ko.utils.arrayFilter(locations, function(place) {
-    		if (place.name.toLowerCase().indexOf(self.query().toLowerCase()) >=0) {
-    			return true;
-    		}
+        for (var i = 0; i < locations.length; i++) {
+            locations[i].marker.setVisible(true);
+        }
+        return ko.utils.arrayFilter(locations, function(place) {
+            if (place.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
+                return true;
+            }
 
-    		place.marker.setVisible(false);
-    		return false;
-    	});
+            place.marker.setVisible(false);
+            return false;
+        });
     });
 
 };
@@ -87,24 +93,34 @@ function initMap() {
 
     var infowindow = new google.maps.InfoWindow();
 
-    var marker;
+    //var marker;
 
 
 
     function createMarker(latlng, html) {
         html = '<h3>' + locations[i].name + '</h3>' + locations[i].address;
         latlng = new google.maps.LatLng(locations[i].lat, locations[i].long);
-        marker = new google.maps.Marker({
-            position: latlng,
+        var marker = new google.maps.Marker({
             map: map,
-            
+            animation: google.maps.Animation.DROP,
+            position: latlng
         });
 
         google.maps.event.addListener(marker, 'click', function() {
             infowindow.setContent(html);
             infowindow.open(map, this);
+            toggleBounce(marker);
         });
         return marker;
+    }
+
+    function toggleBounce(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() { marker.setAnimation(null); }, 650);
+        }
     }
 
 
