73d1c53e4bc394c28074dbc43e7e1be2ad758465
matosb2 <justmats@gmail.com>
2016-08-31 19:30:51 -0400

    Get yelp search working

--- src/js/main.js
+++ src/js/main.js
@@ -4,41 +4,47 @@ var locations = [{
     address: "600 Cape May St, Harrison, NJ 07029",
     lat: 40.737046,
     long: -74.150361,
-    marker: ''
+    marker: '',
+    yelpWeb: "https://api.yelp.com/v2/business/red-bull-arena-harrison"
 }, {
     name: "MetLife Stadium",
     address: "1 MetLife Stadium Dr, East Rutherford, NJ 07073",
     lat: 40.813091,
     long: -74.074209,
-    marker: ''
+    marker: '',
+    yelpWeb: "https://api.yelp.com/v2/business/metlife-stadium-east-rutherford"
 }, {
     name: "World Trade Center",
-    address: "285 Fulton St, New York, NY 10007",
+    address: "1 World Trade Center, New York, NY 10007",
     lat: 40.713175,
     long: -74.013104,
-    marker: ''
+    marker: '',
+    yelpWeb: "https://api.yelp.com/v2/business/one-world-trade-center-new-york-4"
 }, {
     name: "Zeppelin Hall Biergarten",
     address: "88 Liberty View Dr, Jersey City, NJ 07302",
     lat: 40.715120,
     long: -74.046754,
-    marker: ''
+    marker: '',
+    yelpWeb: "https://api.yelp.com/v2/business/zeppelin-hall-biergarten-and-restaurant-jersey-city-2"
 }, {
     name: "Prudential Center",
     address: "25 Lafayette St, Newark, NJ 07102",
     lat: 40.733617,
     long: -74.171150,
-    marker: ''
+    marker: '',
+    yelpWeb: "https://api.yelp.com/v2/business/prudential-center-newark"
 }, {
     name: "Madison Square Garden",
-    address: "4 Pennsylvania Plaza, New York, NY 10001",
+    address: "7th Ave & 32nd St, New York, NY 10001",
     lat: 40.750691,
     long: -73.993476,
-    marker: ''
+    marker: '',
+    yelpWeb: "https://api.yelp.com/v2/business/madison-square-garden-new-york"
 }];
 
 
-//VIEW 
+//VIEW
 var Location = function(data) {
     this.name = ko.observable(data.name);
     /*this.address = ko.observable(data.address);
@@ -93,6 +99,52 @@ var viewModel = function() {
 
 };
 
+function nonce_generate() {
+        return (Math.floor(Math.random() * 1e12).toString());
+    }
+    var yelpAPI = function (i) {
+    var yelp_url = locations[i].yelpWeb;
+
+    var parameters = {
+        oauth_consumer_key: 'AOsWUWqrkWd3Lx9RHt4ihA',
+        oauth_token: 'rqRj4BFQ1xVBEut_57pPedSonmLjkyde',
+        oauth_nonce: nonce_generate(),
+        oauth_timestamp: Math.floor(Date.now() / 1000),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version: '1.0',
+        callback: 'cb', // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+    	  ll: '40.753011, -74.128069',
+    	  radius: 40000,
+    	  limit: 5
+    };
+
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, 'bVxTwnXgkOCAg5Kfhrw7eWEthu8', 'cK98_xpt5iXoBJVyeysJzw5Ypxk');
+    parameters.oauth_signature = encodedSignature;
+
+    var settings = {
+        url: yelp_url,
+        data: parameters,
+        cache: true, // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+        dataType: 'jsonp',
+        success: function(results) {
+          // Do stuff with results
+          locations[i].url = results.url;
+          locations[i].rating_img_large_url = results.rating_img_url_large;
+          locations[i].snippet_text = results.snippet_text;
+        console.log(locations[i]);
+        },
+        error: function() {
+            // Do stuff on fail
+        }
+    };
+
+    // Send AJAX query via jQuery library.
+    $.ajax(settings);
+  };
+    
+  for(var i = 0; i < locations.length; i++) {
+    yelpAPI(i);
+  }
 
 // Main map function that zooms in and centers it at specific location due to the given
 // coordinates.  Also displays the map in the respective div.
@@ -117,11 +169,11 @@ function initMap() {
             position: latlng
         });
 
-        // When marker gets clicked on it toggles bouncing animation and info window pops up 
+        // When marker gets clicked on it toggles bouncing animation and info window pops up
         // on map with html object information from the createMarker function.
         google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(html);
-            infowindow.open(map, this);
+            //infowindow.setContent(html);
+            //infowindow.open(map, this);
             toggleBounce(marker);
         });
         return marker;
@@ -139,54 +191,20 @@ function initMap() {
     }
 
     // Loop that iterates through each object in the locations array.  Marker property stores coordinates
-    // for exact location for each object.  Because createMarker function is called it will display each 
+    // for exact location for each object.  Because createMarker function is called it will display each
     // and every marker in locations on the map.
     for (i = 0; i < locations.length; i++) {
+        
         locations[i].marker = createMarker(new google.maps.LatLng(locations[i].lat, locations[i].long));
     }
-
-
 
     //  Activate knockout bindings
     ko.applyBindings(new viewModel());
 
+
     
-    function nonce_generate() {
-        return (Math.floor(Math.random() * 1e12).toString());
-    }
+    
+    
+}
 
-    var yelp_url = 'http://api.yelp.com/v2/search';
 
-    var parameters = {
-        oauth_consumer_key: 'AOsWUWqrkWd3Lx9RHt4ihA',
-        oauth_token: 'rqRj4BFQ1xVBEut_57pPedSonmLjkyde',
-        oauth_nonce: nonce_generate(),
-        oauth_timestamp: Math.floor(Date.now() / 1000),
-        oauth_signature_method: 'HMAC-SHA1',
-        oauth_version: '1.0',
-        callback: 'cb', // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-    	location: 'New York',
-    	term: 'food',
-    	limit: 1
-    };
-
-    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, 'bVxTwnXgkOCAg5Kfhrw7eWEthu8', 'cK98_xpt5iXoBJVyeysJzw5Ypxk');
-    parameters.oauth_signature = encodedSignature;
-
-    var settings = {
-        url: yelp_url,
-        data: parameters,
-        cache: true, // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-        dataType: 'jsonp',
-        success: function(results) {
-            // Do stuff with results
-            console.log(results);
-        },
-        error: function() {
-            // Do stuff on fail
-        }
-    };
-
-    // Send AJAX query via jQuery library.
-    $.ajax(settings);
-};
